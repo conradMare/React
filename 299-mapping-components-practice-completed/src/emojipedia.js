5891b7b213d41434121bd6ba206a4c95b76f0ef6
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-03 05:34:10 +0200

    2024/07/03(am)
    S36.297_Mapping Data to Components Practice

---
+++ 299-mapping-components-practice-completed/src/emojipedia.js
@@ -0,0 +1,38 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+  id: 5,
+  emoji: "😁️",
+  name: "Grinning Face With Smiling Eyes",
+  meaning: "A beaming face and laughing eyes 😁. This emoji shows a mischievous grin that represents lightheartedness and exuberance. It is often used to show that you are happy about an event, excited, or just a little embarrassed. In conversation, this emoji can be used to express positive feelings and enthusiasm, whether about personal achievements or joyful expectations. Joking and cheerfulness are other meanings associated with this emoji. Finally, it symbolizes the ability to not take yourself too seriously and to make others smile."
+  }
+];
+
+export default emojipedia;
