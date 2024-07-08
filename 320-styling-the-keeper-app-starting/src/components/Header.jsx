af0786a61d8cf7423c4b5e64d52efc5345edee39
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-08 09:09:01 +0200

    2024/07/08(am)
    S36.319__React Dependencies & Styling the Keeper App

---
+++ 320-styling-the-keeper-app-starting/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+
+function Header() {
+  return (
+    <header>
+      <h1><EditNoteOutlinedIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
